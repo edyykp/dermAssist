c4cb69cecaa1c382d2dff2b0bd0dc55f7f9d57ff
Eduard <eduard.c.stoica10@gmail.com>
2026-03-28 18:15:34 +0200

    add ktfmt formatter on pre-commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,9 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktfmt) apply false
+}
+
+subprojects {
+    apply(plugin = "com.ncorti.ktfmt.gradle")
 }
